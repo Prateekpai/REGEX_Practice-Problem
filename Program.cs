13ef40ddb41c33e99db84916f8fb8456a824d64f
Prateekpai <prateekvasanthpai@gmail.com>
2022-04-10 17:53:20 +0530

    Enter Valid Password Which Contains minimum 8 Characters

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace REGEXDemo
+{
+    public class Pattern
+    {
+        public static string Regex_Pattern = "[A-Za-z]{8,}";
+        public bool validatePincode(string Pincode)
+        {
+            return Regex.IsMatch(Pincode, Regex_Pattern);
+        }
+    }
+    public class REGEX
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("The Password shall contain atleast 8 Character");
+            Console.WriteLine("Enter Password ");
+            string Pincode = Console.ReadLine();
+
+            Pattern pattern = new Pattern();
+            Console.WriteLine("....................................");
+            if (pattern.validatePincode(Pincode))
+                Console.WriteLine("Your Password {0} is saved", Pincode);
+            else
+                Console.WriteLine("Enter Valid Password");
+        }
+    }
+}
